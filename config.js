834c591adf31970e520819dcea190e4751ad85dd
황수희 <suhui0321@gmail.com>
2026-08-19 02:08:18 +0900

    Connect Supabase project

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Supabase 프로젝트의 공개 연결 정보를 입력합니다.
 // 주의: service_role 또는 secret 키는 절대 브라우저 코드에 넣지 않습니다.
 window.BAKEWEB_CONFIG = Object.freeze({
-  // 예시: https://xxxxxxxxxxxx.supabase.co
-  supabaseUrl: '',
+  // BAKEWEB 견적서 데이터베이스의 공개 프로젝트 주소입니다.
+  supabaseUrl: 'https://wptzwuckcekjuyedzcdc.supabase.co',
   // Supabase 대시보드의 Publishable key 또는 legacy anon key를 입력합니다.
-  supabasePublishableKey: ''
+  supabasePublishableKey: 'sb_publishable_3fEA0Gyy3C4s9FMkCqT-Qw_VABvT1wy'
 });
